a08b57d082113f5b43619bb24230b89891659e0c
Patrick Le <patricktle924@gmail.com>
2025-04-17 22:11:43 -0700

    Add Links to Navbar + Change Styling

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,14 @@
+import {Link, NavLink} from 'react-router-dom';
+
 function NavBar() {
 
     return (
-        <nav className="bg-green-300 border-b border-white-300 sticky top-0 z-10">
+        <nav className="bg-cyan-900 border-b border-white-300 sticky top-0 z-10 flex justify-around">
             <div> NavBar</div>
+            <Link className="border-cyan-800 bg-cyan-600 text-white rounded-2xl border-2 m-1 p-2" to="/">Home</Link>
+            <Link className="border-cyan-800 bg-cyan-600 text-white rounded-2xl border-2 m-1 p-2" to="/checkout">Checkout</Link>
+            <Link className="border-cyan-800 bg-cyan-600 text-white rounded-2xl border-2 m-1 p-2" to="/productlisting">Search</Link>
+            <Link className="border-cyan-800 bg-cyan-600 text-white rounded-2xl border-2 m-1 p-2" to="/wishlist">Wishlist</Link>
         </nav>
 
     );
